a81feeecc2f1ed7daf6a03a5130b16f97172db35
Trish Jedrick Coyoca <oreo.ubphrx@gmail.com>
2019-08-19 16:19:44 +0800

    Updated header headline

--- plethora/resources/views/landing/layouts/headline.blade.php
+++ plethora/resources/views/landing/layouts/headline.blade.php
@@ -1,7 +1,7 @@
  <!-- Plethora headline -->
  <div class = "phr-headline">
     <div class="container text-right">
-        <span><i class="fa fa-inbox"></i> plethorahomes@gmail.com</span>
-        <span><i class="fa fa-phone"></i> 0921-7298-758</span>
+        <span><i class="fa fa-inbox"></i> plethorahomesrealty@gmail.com</span>
+        <span><i class="fa fa-phone"></i> N/A</span>
     </div>
 </div>
