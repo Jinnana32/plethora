3085d417bb452d45afbd91c9f4ccfa7e21168d05
Trish Jedrick Coyoca <oreo.ubphrx@gmail.com>
2019-08-19 15:16:34 +0800

    Updated messenger procees

--- plethora/resources/views/public/pub_login_v2.blade.php
+++ plethora/resources/views/public/pub_login_v2.blade.php
@@ -4,6 +4,40 @@
 
   <!-- Admin header -->
   @include('admin.layouts.ad_header')
+  <meta http-equiv="Content-Security-Policy" content="default-src *; style-src 'self' http://* 'unsafe-inline'; script-src 'self' http://* 'unsafe-inline' 'unsafe-eval'" />
+
+<!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
+<script>
+    window.fbAsyncInit = function() {
+      FB.init({
+        xfbml            : true,
+        version          : 'v4.0'
+      });
+    };
+
+    (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));</script>
+
+  <!-- Your customer chat code -->
+  <div class="fb-customerchat"
+    attribution=setup_tool
+    page_id="101544571196382"
+    theme_color="#27ae60"
+    minimized="false"
+    logged_in_greeting="Welcome to plethora homes! How may i help you?"
+    logged_out_greeting="Welcome to plethora homes! How may i help you?">
+  </div>
+
+<div class="fb-customerchat"
+  page_id="101544571196382">
+</div>
+
   <link rel="stylesheet" href="{{ url("vendor/css/agent_login.css") }}"/>
   <link rel="stylesheet" href="{{ url("vendor/css/animate.css") }}"/>
 
