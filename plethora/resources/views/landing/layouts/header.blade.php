479f2ff72bb762cf843d455f7fe561ad5571afd5
Trish Jedrick Coyoca <oreo.ubphrx@gmail.com>
2019-08-19 16:17:06 +0800

    Remove messenger in agent field

--- plethora/resources/views/landing/layouts/header.blade.php
+++ plethora/resources/views/landing/layouts/header.blade.php
@@ -4,39 +4,6 @@
 <meta name="author" content="">
 
 <title>Plethora Realty Homes</title>
-<meta http-equiv="Content-Security-Policy" content="default-src *; style-src 'self' http://* 'unsafe-inline'; script-src 'self' http://* 'unsafe-inline' 'unsafe-eval'" />
-
-<!-- Load Facebook SDK for JavaScript -->
-<div id="fb-root"></div>
-<script>
-    window.fbAsyncInit = function() {
-      FB.init({
-        xfbml            : true,
-        version          : 'v4.0'
-      });
-    };
-
-    (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));</script>
-
-  <!-- Your customer chat code -->
-  <div class="fb-customerchat"
-    attribution=setup_tool
-    page_id="101544571196382"
-    theme_color="#27ae60"
-    minimized="false"
-    logged_in_greeting="Welcome to plethora homes! How may i help you?"
-    logged_out_greeting="Welcome to plethora homes! How may i help you?">
-  </div>
-
-<div class="fb-customerchat"
-  page_id="101544571196382">
-</div>
 
 <!-- Bootstrap core CSS -->
 <link href="{{ url("css/bootstrap.min.css") }}" rel="stylesheet">
